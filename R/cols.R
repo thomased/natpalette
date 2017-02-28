651caaface93e404b8a1d6d5a7b6edab14928719
thomased <thomas.white026@gmail.com>
2017-02-28 15:53:22 +1100

    typo

--- R/cols.R
+++ R/cols.R
@@ -48,7 +48,7 @@ palettes <- list(
   Acripeza = c('#0b0c07', '#ca3703', '#2474c9', '#899273'),
   Chrysiridia = c('#5e8e87', '#e38e13', '#cacb39', '#7ae1f2', '#0827d4', '#793af3', '#aa2132'),
   Delias = c('#fb2d15', '#312c32', '#fdd633', '#e4e2f0'),
-  Furcifur = c('#a2f5fb', '#7dcc7b', '#dbcc55', '#d8894e', '#93331d'),
+  Furcifer = c('#a2f5fb', '#7dcc7b', '#dbcc55', '#d8894e', '#93331d'),
   Maratus = c('#c2370e', '#c7621a', '#d8b54d', '#8ec9eb', '#d1d7ef'),
   Trichoglossus = c('#517b39', '#b2ba31', '#8271b7', '#fdb81d', '#cd3f35'),
   Tulipa = c('#e61229', '#ff5e20', '#d46790', '#e8b413', '#e6d0a7')
