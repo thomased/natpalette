2e113bc7c5c10b339b6a7045bc16feb200f70429
thomased <thomas.white026@gmail.com>
2017-02-28 11:31:43 +1100

    Initial

---
+++ R/cols.R
@@ -0,0 +1,65 @@
+#' A 'natural' palette generator
+#'
+#' Some fairly impractical colour palettes inspired by nature.
+#'
+#' @param n Number of colours. If omitted, uses all colours.
+#' @param name The desired palette. One of:
+#' \itemize{
+#'   \item \code{Acripeza}: The mountain katydid \emph{Acripeza reticulata}.
+#'   \item \code{Chrysiridia}: The Madagascan sunset moth \emph{Chrysiridia rhipheus}.
+#'   \item \code{Delias}: The Phillippine jezabel \emph{Delias henningia}.
+#'   \item \code{Maratus}: The peacock spider \emph{Maratus volans}.
+#'   \item \code{Trichoglossus}: The rainbow lorikeet \emph{Trichoglossus haematodus}.
+#'   \item \code{Tulipa}: Tulips \emph{Tulipa gesneriana}.
+#'   }
+#' @return A vector of colours.
+#' @export
+#' @keywords colours
+#' @examples
+#' nat_palette('Maratus')
+#' nat_palette('Tulipa')
+#'
+nat_palette <- function(name, n) {
+
+  pal <- palettes[[name]]
+  if(is.null(pal))
+    stop('No such palette.')
+
+  if(missing(n)){
+    n <- length(pal)
+  }
+
+  if(n > length(pal)){
+    stop('Not enough colours in the requested palette.')
+  }
+
+  # Data attributes
+  out <- pal[1:n]
+  structure(out, class = 'palette', name = name)
+}
+
+#' Palettes
+#'
+#' Colours. Use \code{\link{nat_palette}} to build palettes.
+#'
+#' @export
+palettes <- list(
+  Acripeza = c('#0b0c07', '#ca3703', '#2474c9', '#899273'),
+  Chrysiridia = c('#5e8e87', '#e38e13', '#cacb39', '#7ae1f2', '#0827d4', '#793af3', '#aa2132'),
+  Delias = c('#fb2d15', '#312c32', '#fdd633', '#e4e2f0'),
+  Maratus = c('#c2370e', '#d8b54d', '#c7621a', '#577d56', '#8ec9eb', '#d1d7ef'),
+  Trichoglossus = c('#517b39', '#b2ba31', '#8271b7', '#fdb81d', '#cd3f35'),
+  Tulipa = c('#e61229', '#ff5e20', '#d46790', '#e8b413', '#e6d0a7')
+  )
+
+
+#' @export
+#' @import graphics
+print.palette <- function(x, ...){
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1, as.matrix(1:n), col = x,
+    ylab = '', xaxt = 'n', yaxt = 'n', bty = 'n')
+}
---
+++ R/natpalette.R
@@ -0,0 +1,6 @@
+#' @title natpalette
+#' @name natpalette
+#' @docType package
+#' @details colour palettes inspired by nature
+#' @description colour palettes inspired by nature
+NULL
